46446a753faeb7fb98617e094dd3bdc0e5b98e90
Kostia Palchyk <kosich@users.noreply.github.com>
2021-04-29 21:00:49 +0300

    Observable example

--- test.js
+++ test.js
@@ -1,3 +1,35 @@
-42
-  |> _ + '!'
-  |> console.log(_)
+// Core
+let Observable =
+  producer =>
+  subscriber =>
+  producer(subscriber);
+
+let subscribe =
+  subscriber =>
+  observable =>
+  observable(subscriber);
+
+// Producers
+let timer = interval => Observable(subscriber => {
+  let i = 0;
+  let id = setInterval(() => subscriber(i++), interval);
+  return () => clearInterval(id);
+});
+
+// Operators
+let filter = fn => source => Observable(subscriber => 
+  source(value => fn(value) && subscriber(value))
+)
+
+let map = fn => source => Observable(subscriber => 
+  source(value => subscriber(fn(value)))
+)
+
+// WILDLANDS
+let unsub = timer(100)
+  |> filter(x => x % 2)
+  |> map(x => x + '!')
+  |> subscribe(console.log)
+
+
+setTimeout(unsub, 1000);
