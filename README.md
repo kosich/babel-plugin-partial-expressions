2f30a034bc5503572d6336eaba4cf3d321b1c589
Kostia Palchyk <kosich@users.noreply.github.com>
2021-05-14 23:01:54 +0300

    compile target and updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Partial Expressions - Babel plugin
 
-Try it in this **[babel playground](https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=usage&corejs=3.6&spec=false&loose=false&code_lz=CwJgUABBA-B8EH0IGoIHICEbI3gYwHsA7AZwIBsBTAOnIIHMBuIA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=true&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Cstage-0%2Cstage-1&prettier=false&targets=&version=7.13.17&externalPlugins=%40kosich%2Fbabel-plugin-partial-expressions-experiment%400.0.1)**.
+Try it in this **[babel playground](https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=usage&corejs=3.6&spec=false&loose=false&code_lz=DYUwLgBAlgdgxhAvBAfhA-hA1BAjAbgCg4B7GAZxNADpgSBzACljkYBZcBKTow0SAA5QBIACZIIbAAwQAPgD4M2CACYipClRC0GjISNE8gA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=false&presets=stage-0%2Cstage-1&prettier=false&targets=&version=7.13.17&externalPlugins=%40kosich%2Fbabel-plugin-partial-expressions-experiment%400.0.4)**.
 
 Or install it via
 
@@ -14,10 +14,16 @@ _NOTE: put it before pipeline operator plugins, if you use those._
 
 ## About
 
-Partial expressions is a syntax sugar to simplify writing expression that are awating one additional variable, e.g.:
+Partial expressions is a syntax sugar to simplify writing expression that are awating one additional variable.
+
+Syntax:
+- `_` is a placeholder for the value
+- `~` is used to mark partial expression root and is required whenever we use `_` (except for RHS pipes)
+
+Examples:
 
 ```js
-let bang = _ + '!';
+let bang = ~ _ + '!';
 // equals to
 let bang = x => x + '!';
 ```
@@ -25,7 +31,7 @@ let bang = x => x + '!';
 As a partially applied function:
 
 ```js
-let bangLog = console.log(_, '!');
+let bangLog = ~ console.log(_, '!');
 // equals to
 let bangLog = x => console.log(x, '!');
 ```
@@ -33,24 +39,14 @@ let bangLog = x => console.log(x, '!');
 And as a context of application:
 
 ```js
-let toBase16 = _.toString(16);
+let toBase16 = ~ _.toString(16);
 // equals to
 let toBase16 = x => x.toString(16);
 ```
 
 It is also compatible with the [pipeline operator](https://github.com/tc39/proposal-pipeline-operator):
 
-I tried to cover two special cases for pipes.
-
-When the partial expression is in the **subject** of piping:
-
-```js
-let fn = _ |> increment |> console.log;
-// equals to
-let fn = x => (x |> increment |> console.log);
-```
-
-And when it's in a **pipe**:
+I tried to cover a special case for **pipes**, when `_` is in RHS of a pipe. E.g:
 
 ```js
 let value = 42 |> _ + 1 |> console.log;
@@ -58,9 +54,7 @@ let value = 42 |> _ + 1 |> console.log;
 let value = 42 |> (x => x + 1) |> console.log;
 ```
 
-.
-
-Similarly, we could do partial application:
+Similarly, we can do partial application:
 
 ```js
 42 |> console.log(_, '!');
@@ -82,13 +76,10 @@ And as a context:
 
 2. Expression boudaries are limited by following:
 
-    2.1 root of an expression    
-    `let a = _ + '!'` equals `let a = x => x + '!'`
+    2.1 `~` operator
+    `let a = ~ _ + '!'` equals `let a = x => x + '!'`
 
-    2.2 body of a function    
-    `let b = x => _ + '!'` equals `let b = x => n => n + '!'`
-
-    2.3 pipe in pipeline operator    
+    2.2 RHS in pipeline operator    
     `42 |> _ + 1 |> console.log` equals `42 |> (x => x + 1) |> console.log`
 
 ## The End 🙂
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const types_1 = require("@babel/types");
-const pipeOperatorSigil = '|>';
-const placeholderSigil = '_';
-const markerSigil = '~';
+var types_1 = require("@babel/types");
+var pipeOperatorSigil = '|>';
+var placeholderSigil = '_';
+var markerSigil = '~';
 function BabelPluginPartialExpressions() {
-    let config = {
+    var config = {
         visitor: {
-            UnaryExpression,
-            BinaryExpression,
+            UnaryExpression: UnaryExpression,
+            BinaryExpression: BinaryExpression,
         }
     };
     return config;
@@ -17,14 +17,14 @@ exports.default = BabelPluginPartialExpressions;
 // Need to traverse pipeline expressions `a |> b`
 // before pipeline-proposal plugin does
 // because partial expression might have other priority
-let BinaryExpression = (path) => {
+var BinaryExpression = function (path) {
     if (path.node.operator != pipeOperatorSigil) {
         return;
     }
     // here we prioritize partial expression visitors,
     // so that they are prepared before |> pipes are applied
     path.parentPath.traverse({
-        UnaryExpression
+        UnaryExpression: UnaryExpression
     });
     path.parentPath.traverse({
         Identifier: createPlaceholderTraverser(path.get('right'))
@@ -36,13 +36,13 @@ let BinaryExpression = (path) => {
 // let a = ~ 42 + _;
 // equals to
 // let a = x => 42 + x;
-let UnaryExpression = (path) => {
+var UnaryExpression = function (path) {
     if (path.node.operator !== markerSigil) {
         return;
     }
     // bubble while expression position is equal to current ~ position
     // then turn it into an arrow function
-    let replacePath = path;
+    var replacePath = path;
     while (hasEqualStartLocation(replacePath, replacePath.parentPath)) {
         replacePath.assertExpression();
         replacePath = replacePath.parentPath;
@@ -52,14 +52,14 @@ let UnaryExpression = (path) => {
     // replace the ~ group w/ a function
     replacePath.parentPath.traverse({
         Identifier: createPlaceholderTraverser(replacePath),
-        UnaryExpression
+        UnaryExpression: UnaryExpression
     });
 };
 // Identifier visitor with a defined "root"
 // this root marks place where replacement should happen
 // TODO: test nested expressions
-let createPlaceholderTraverser = (root) => (path) => {
-    let { scope, node } = path;
+var createPlaceholderTraverser = function (root) { return function (path) {
+    var scope = path.scope, node = path.node;
     if (node.name != placeholderSigil) {
         return;
     }
@@ -68,7 +68,7 @@ let createPlaceholderTraverser = (root) => (path) => {
         return;
     }
     // find the level where we substitute the expression
-    let replacePoint = path;
+    var replacePoint = path;
     while (replacePoint && replacePoint !== root && !isPipeRight(replacePoint)) {
         replacePoint = replacePoint.parentPath;
     }
@@ -77,16 +77,17 @@ let createPlaceholderTraverser = (root) => (path) => {
     }
     // replacing _ with an identifier
     // that will later be used as function argument
-    let tempUid = scope.generateUidIdentifier('part');
+    var tempUid = scope.generateUidIdentifier('part');
     path.replaceWith(tempUid);
     replacePoint.replaceWith(types_1.parenthesizedExpression(types_1.arrowFunctionExpression([tempUid], types_1.parenthesizedExpression(replacePoint.node))));
-};
+}; };
 function hasEqualStartLocation(a, b) {
-    return a.node.loc?.start.line == b.node.loc?.start.line
-        && a.node.loc?.start.column == b.node.loc?.start.column;
+    var _a, _b, _c, _d;
+    return ((_a = a.node.loc) === null || _a === void 0 ? void 0 : _a.start.line) == ((_b = b.node.loc) === null || _b === void 0 ? void 0 : _b.start.line)
+        && ((_c = a.node.loc) === null || _c === void 0 ? void 0 : _c.start.column) == ((_d = b.node.loc) === null || _d === void 0 ? void 0 : _d.start.column);
 }
 function isPipeRight(path) {
-    let parent = path?.parentPath;
+    var parent = path === null || path === void 0 ? void 0 : path.parentPath;
     return parent
         && parent.isBinaryExpression()
         && parent.node.operator == pipeOperatorSigil
